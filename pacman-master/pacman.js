fa2ff21ed85176e8b00e2b5aa7848f362ef02243
youssef belghith <youssef.belghith@ensi-uma.tn>
2026-06-17 09:18:21 +0100

    update

--- pacman-master/pacman.js
+++ pacman-master/pacman.js
@@ -1,14 +1,47 @@
 let board;
-const rowCount =21;
+const rowCount = 21;
 const columnCount = 19;
-const titlesize=32;
-const boardWidth= columnCount*titlesize;
-const boardHeight= rowCount*titlesize;
+const tileSize = 32;
+const boardWidth = columnCount*tileSize;
+const boardHeight = rowCount*tileSize;
 let context;
 
-window.onload =function(){
-    board=document.getElementById("board");
-    board.height=boardHeight;
-    board.width=boardWidth;
-    context = board.getcontext("2d");
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+let pacmanUpImage;
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d");
+}
+
+function loadImages() {
+    wallImage = new Image();
+    wallImage.src = "./wall.png";
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = "./blueGhost.png";
+    orangeGhostImage = new Image();
+    orangeGhostImage.src = "./orangeGhost.png"
+    pinkGhostImage = new Image()
+    pinkGhostImage.src = "./pinkGhost.png";
+    redGhostImage = new Image()
+    redGhostImage.src = "./redGhost.png";
+
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = "./pacmanUp.png";
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "./pacmanDown.png";
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "./pacmanLeft.png";
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = "./pacmanRight.png";
 }
